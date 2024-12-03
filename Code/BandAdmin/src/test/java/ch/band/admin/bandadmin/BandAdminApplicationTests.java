14d98ad9d9350d6b4b978f70efdc29ee0a7073d2
Quentin Herren <quentin.herren@ergon.ch>
2024-12-03 09:01:31 +0100

    AUB-12: adds first microservice

---
+++ Code/BandAdmin/src/test/java/ch/band/admin/bandadmin/BandAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.band.admin.bandadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
